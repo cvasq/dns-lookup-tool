9e430f75102fac53c95afccd00ab77b64f69f5d0
Carlos Vasquez <carlos@cobranix.com>
2019-08-02 00:15:25 -0400

    Debugging env var

--- ui/vue.config.js
+++ ui/vue.config.js
@@ -1,6 +1,20 @@
 module.exports = {                                                                                                                                                                                                                                                                                                           
+} 
+
+var webpack = require('webpack')
+	
+module.exports = {
   publicPath: process.env.VUE_APP_BASE_PATH === undefined || process.env.VUE_APP_BASE_PATH === null                                                                                                                                                                                                                          
     ? '/'
     : process.env.VUE_APP_BASE_PATH,                                                                                                                                                                                                                                                                                         
-  runtimeCompiler: true                                                                                                                                                                                                                                                                                                      
-} 
+  runtimeCompiler: true,
+  configureWebpack: {
+    plugins: [
+      new webpack.DefinePlugin({
+        'process.env': {
+          'VUE_APP_WS_URL': JSON.stringify(process.env.VUE_APP_WS_URL)
+        }
+      })
+    ]
+  }
+}
